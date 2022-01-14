e3189a7697c65eb1723663a038b63ce8f36575df
tsmr <thetsmr@gmail.com>
2022-01-14 09:22:12 +0100

    Update headers & int unsigned

--- sql/empty-1.9.0.sql
+++ sql/empty-1.9.0.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS `glpi_plugin_manageentities_contracts`;
 CREATE TABLE `glpi_plugin_manageentities_contracts` (
-   `id` int(11) NOT NULL auto_increment,
-   `contracts_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_contracts (id)',
-   `entities_id` int(11) NOT NULL default '0',
-   `is_default` tinyint(1) NOT NULL default '0',
-   `management` tinyint(1) NOT NULL default '0' COMMENT 'for the management mode (quarterly or annual or not)',
-   `contract_type` tinyint(1) NOT NULL default '0' COMMENT 'for the contract type (hour, intervention, unlimited or not)',
+   `id` int unsigned NOT NULL auto_increment,
+   `contracts_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_contracts (id)',
+   `entities_id` int unsigned NOT NULL default '0',
+   `is_default` tinyint NOT NULL default '0',
+   `management` tinyint NOT NULL default '0' COMMENT 'for the management mode (quarterly or annual or not)',
+   `contract_type` tinyint NOT NULL default '0' COMMENT 'for the contract type (hour, intervention, unlimited or not)',
    `date_signature` date default NULL,
    `date_renewal` date default NULL,
    PRIMARY KEY  (`id`),
@@ -16,10 +16,10 @@ CREATE TABLE `glpi_plugin_manageentities_contracts` (
 
 DROP TABLE IF EXISTS `glpi_plugin_manageentities_contacts`;
 CREATE TABLE `glpi_plugin_manageentities_contacts` (
-   `id` int(11) NOT NULL auto_increment,
-   `contacts_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_contacts (id)',
-   `entities_id` int(11) NOT NULL default '0',
-   `is_default` tinyint(1) NOT NULL default '0',
+   `id` int unsigned NOT NULL auto_increment,
+   `contacts_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_contacts (id)',
+   `entities_id` int unsigned NOT NULL default '0',
+   `is_default` tinyint NOT NULL default '0',
    PRIMARY KEY  (`id`),
    UNIQUE KEY `unicity` (`contacts_id`,`entities_id`),
    KEY `contacts_id` (`contacts_id`),
@@ -28,8 +28,8 @@ CREATE TABLE `glpi_plugin_manageentities_contacts` (
 
 DROP TABLE IF EXISTS `glpi_plugin_manageentities_profiles`;
 CREATE TABLE `glpi_plugin_manageentities_profiles` (
-   `id` int(11) NOT NULL auto_increment,
-   `profiles_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_profiles (id)',
+   `id` int unsigned NOT NULL auto_increment,
+   `profiles_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_profiles (id)',
    `manageentities` char(1) collate utf8_unicode_ci default NULL,
    `cri_create` char(1) collate utf8_unicode_ci default NULL,
    PRIMARY KEY  (`id`),
@@ -38,23 +38,23 @@ CREATE TABLE `glpi_plugin_manageentities_profiles` (
 
 DROP TABLE IF EXISTS `glpi_plugin_manageentities_preferences`;
 CREATE TABLE `glpi_plugin_manageentities_preferences` (
-   `id` int(11) NOT NULL auto_increment,
-   `users_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_users (id)',
-   `show_on_load` int(11) NOT NULL default '0',
+   `id` int unsigned NOT NULL auto_increment,
+   `users_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_users (id)',
+   `show_on_load` int unsigned NOT NULL default '0',
    PRIMARY KEY  (`id`),
    KEY `users_id` (`users_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 DROP TABLE IF EXISTS `glpi_plugin_manageentities_configs`;
 CREATE TABLE `glpi_plugin_manageentities_configs` (
-   `id` int(11) NOT NULL auto_increment,
-   `backup` int(11) NOT NULL default '0',
-   `documentcategories_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_documentcategories (id)',
-   `useprice` tinyint(1) NOT NULL default '1' COMMENT 'default for yes',
-   `hourorday` tinyint(1) NOT NULL default '0' COMMENT 'default for day',
-   `hourbyday` int(11) NOT NULL default '0' COMMENT 'if hourorday == 0 then must be different of 0',
-   `needvalidationforcri` tinyint(1) NOT NULL default '0' COMMENT 'only CRI with validated ticket are taking into account for consumption calculation',
-   `use_publictask` tinyint(1) NOT NULL default '0' COMMENT 'default for no',
+   `id` int unsigned NOT NULL auto_increment,
+   `backup` int unsigned NOT NULL default '0',
+   `documentcategories_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_documentcategories (id)',
+   `useprice` tinyint NOT NULL default '1' COMMENT 'default for yes',
+   `hourorday` tinyint NOT NULL default '0' COMMENT 'default for day',
+   `hourbyday` int unsigned NOT NULL default '0' COMMENT 'if hourorday == 0 then must be different of 0',
+   `needvalidationforcri` tinyint NOT NULL default '0' COMMENT 'only CRI with validated ticket are taking into account for consumption calculation',
+   `use_publictask` tinyint NOT NULL default '0' COMMENT 'default for no',
    PRIMARY KEY  (`id`),
    KEY `documentcategories_id` (`documentcategories_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -63,7 +63,7 @@ INSERT INTO `glpi_plugin_manageentities_configs` (`id`,`backup`,`documentcategor
 
 DROP TABLE IF EXISTS `glpi_plugin_manageentities_critypes`;
 CREATE TABLE `glpi_plugin_manageentities_critypes` (
-   `id` int(11) NOT NULL auto_increment,
+   `id` int unsigned NOT NULL auto_increment,
    `name` varchar(255) collate utf8_unicode_ci default NULL,
    `comment` text collate utf8_unicode_ci,
    PRIMARY KEY  (`id`),
@@ -72,9 +72,9 @@ CREATE TABLE `glpi_plugin_manageentities_critypes` (
 
 DROP TABLE IF EXISTS `glpi_plugin_manageentities_criprices`;
 CREATE TABLE `glpi_plugin_manageentities_criprices` (
-   `id` int(11) NOT NULL auto_increment,
-   `entities_id` int(11) NOT NULL default '0',
-   `plugin_manageentities_critypes_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_plugin_manageentities_critypes (id)',
+   `id` int unsigned NOT NULL auto_increment,
+   `entities_id` int unsigned NOT NULL default '0',
+   `plugin_manageentities_critypes_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_plugin_manageentities_critypes (id)',
    `price` decimal(20,4) NOT NULL default '0.0000',
    PRIMARY KEY  (`id`),
    KEY `entities_id` (`entities_id`),
@@ -83,12 +83,12 @@ CREATE TABLE `glpi_plugin_manageentities_criprices` (
 
 DROP TABLE IF EXISTS `glpi_plugin_manageentities_contractdays`;
 CREATE TABLE `glpi_plugin_manageentities_contractdays` (
-   `id` int(11) NOT NULL auto_increment,
-   `entities_id` int(11) NOT NULL default '0',
+   `id` int unsigned NOT NULL auto_increment,
+   `entities_id` int unsigned NOT NULL default '0',
    `name` varchar(255) collate utf8_unicode_ci default NULL,
-   `plugin_manageentities_critypes_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_plugin_manageentities_critypes (id)',
-   `contracts_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_contracts (id)',
-   `plugin_manageentities_contractstates_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_plugin_manageentities_contractstates (id)',
+   `plugin_manageentities_critypes_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_plugin_manageentities_critypes (id)',
+   `contracts_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_contracts (id)',
+   `plugin_manageentities_contractstates_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_plugin_manageentities_contractstates (id)',
    `begin_date` date default NULL,
    `end_date` date default NULL,
    `nbday` decimal(20,2) default '0.00',
@@ -102,9 +102,9 @@ CREATE TABLE `glpi_plugin_manageentities_contractdays` (
 
 DROP TABLE IF EXISTS `glpi_plugin_manageentities_critechnicians`;
 CREATE TABLE `glpi_plugin_manageentities_critechnicians` (
-   `id` int(11) NOT NULL auto_increment,
-   `tickets_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_tickets (id)',
-   `users_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_users (id)',
+   `id` int unsigned NOT NULL auto_increment,
+   `tickets_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_tickets (id)',
+   `users_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_users (id)',
    PRIMARY KEY  (`id`),
    KEY `tickets_id` (`tickets_id`),
    KEY `users_id` (`users_id`)
@@ -112,16 +112,16 @@ CREATE TABLE `glpi_plugin_manageentities_critechnicians` (
 
 DROP TABLE IF EXISTS `glpi_plugin_manageentities_cridetails`;
 CREATE TABLE `glpi_plugin_manageentities_cridetails` (
-   `id` int(11) NOT NULL auto_increment,
-   `entities_id` int(11) NOT NULL default '0',
+   `id` int unsigned NOT NULL auto_increment,
+   `entities_id` int unsigned NOT NULL default '0',
    `date` date default NULL,
-   `documents_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_documents (id)',
-   `plugin_manageentities_critypes_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_plugin_manageentities_critypes (id)',
-   `withcontract` int(11) NOT NULL default '0',
-   `contracts_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_contracts (id)',
+   `documents_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_documents (id)',
+   `plugin_manageentities_critypes_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_plugin_manageentities_critypes (id)',
+   `withcontract` int unsigned NOT NULL default '0',
+   `contracts_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_contracts (id)',
    `realtime` decimal(20,2) default '0.00',
    `technicians` varchar(255) collate utf8_unicode_ci default NULL,
-   `tickets_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_tickets (id)',
+   `tickets_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_tickets (id)',
    PRIMARY KEY  (`id`),
    KEY `entities_id` (`entities_id`),
    KEY `documents_id` (`documents_id`),
@@ -132,9 +132,9 @@ CREATE TABLE `glpi_plugin_manageentities_cridetails` (
 
 DROP TABLE IF EXISTS `glpi_plugin_manageentities_contractstates`;
 CREATE TABLE `glpi_plugin_manageentities_contractstates` (
-   `id` int(11) NOT NULL auto_increment,
+   `id` int unsigned NOT NULL auto_increment,
    `name` varchar(255) collate utf8_unicode_ci default NULL,
-   `is_active` tinyint(1) NOT NULL default '0',
+   `is_active` tinyint NOT NULL default '0',
    `comment` text collate utf8_unicode_ci,
    PRIMARY KEY  (`id`),
    KEY `name` (`name`),
@@ -143,9 +143,9 @@ CREATE TABLE `glpi_plugin_manageentities_contractstates` (
 
 DROP TABLE IF EXISTS `glpi_plugin_manageentities_taskcategories`;
 CREATE TABLE `glpi_plugin_manageentities_taskcategories` (
-   `id` int(11) NOT NULL auto_increment,
-   `taskcategories_id` int(11) NOT NULL default '0' COMMENT 'RELATION to  glpi_taskcategories (id)',
-   `is_usedforcount` tinyint(1) NOT NULL default '0',
+   `id` int unsigned NOT NULL auto_increment,
+   `taskcategories_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to  glpi_taskcategories (id)',
+   `is_usedforcount` tinyint NOT NULL default '0',
    PRIMARY KEY  (`id`),
    KEY `taskcategories_id` (`taskcategories_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
