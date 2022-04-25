085263a9a464cdff2943f9ba1a7b3accc3011e6f
tsmr <thetsmr@gmail.com>
2022-04-25 10:26:19 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_MANAGEENTITIES_VERSION', '4.0.0-rc1');
+define('PLUGIN_MANAGEENTITIES_VERSION', '4.0.0');
 
 if (!defined("PLUGIN_MANAGEENTITIES_DIR")) {
    define("PLUGIN_MANAGEENTITIES_DIR", Plugin::getPhpDir("manageentities"));
