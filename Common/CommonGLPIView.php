106f08b8213b1a675a503a887f2a883dae12aa48
Thetsmr <thetsmr@gmail.com>
2026-05-26 10:34:51 +0200

    Update HEADERS

--- Common/CommonGLPIView.php
+++ Common/CommonGLPIView.php
@@ -1,32 +1,31 @@
 <?php
+
 /*
- * @version $Id: HEADER 15930 2011-10-30 15:47:55Z tsmr $
  -------------------------------------------------------------------------
- Manageentities plugin for GLPI
- Copyright (C) 2014-2022 by the Manageentities Development Team.
+ manageentities plugin for GLPI
+ Copyright (C) 2017-2026 by the manageentities Development Team.
 
  https://github.com/InfotelGLPI/manageentities
  -------------------------------------------------------------------------
 
  LICENSE
 
- This file is part of Manageentities.
+ This file is part of manageentities.
 
- Manageentities is free software; you can redistribute it and/or modify
+ manageentities is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
 
- Manageentities is distributed in the hope that it will be useful,
+ manageentities is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with Manageentities. If not, see <http://www.gnu.org/licenses/>.
+ along with manageentities. If not, see <http://www.gnu.org/licenses/>.
  --------------------------------------------------------------------------
  */
-
 
 if (!defined('GLPI_ROOT')) {
     die("Sorry. You can't access directly to this file");
