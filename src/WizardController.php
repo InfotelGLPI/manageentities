a269c7229235ea0a80250017f7cefb8d9fe5ccac
Thetsmr <thetsmr@gmail.com>
2026-07-05 11:07:54 +0200

    Fix tests

--- src/WizardController.php
+++ src/WizardController.php
@@ -136,15 +136,6 @@ class WizardController
         if (empty($input['plugin_manageentities_contractstates_id'] ?? 0)) {
             $errors['plugin_manageentities_contractstates_id'] = __('State is required', 'manageentities');
         }
-        try {
-            $config = Config::getInstance();
-            $hourorday = $config->fields['hourorday'] ?? null;
-        } catch (\Throwable $e) {
-            $hourorday = null;
-        }
-        if ($hourorday !== null && $hourorday == Config::DAY && empty((int)($input['contract_type'] ?? 0))) {
-            $errors['contract_type'] = __('Intervention type is required', 'manageentities');
-        }
         return ['valid' => empty($errors), 'errors' => $errors];
     }
 
