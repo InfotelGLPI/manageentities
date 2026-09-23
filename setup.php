e2ab714b2e6ad49c3b919f6dcad515187549dbf0
Thetsmr <thetsmr@gmail.com>
2026-09-23 14:26:16 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  * --------------------------------------------------------------------------
  */
 
-define('PLUGIN_MANAGEENTITIES_VERSION', '4.2.16');
+define('PLUGIN_MANAGEENTITIES_VERSION', '4.2.17');
 
 global $CFG_GLPI;
 
